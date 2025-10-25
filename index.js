6334f377fdb0f1ef38203c48d5aa0f22979e20d8
Manasses <manasseshudson@gmail.com>
2025-10-25 08:50:09 -0300

    s

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ require('dotenv').config();
 app.use(cors());
 
 app.set('view engine', 'ejs');
-app.use(express.static(path.join(__dirname, '/public/assets')));
+app.use(express.static(path.join(__dirname, '/public')));
 app.use(bodyParser.urlencoded({extended: false}))
 app.use(bodyParser.json())
 app.use(express.json({limit: '50mb'}));
